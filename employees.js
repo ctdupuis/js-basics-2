1fea0b771eb2f68947c1f79a2c122c775b294b65
ctdupuis <ctdupuis7@gmail.com>
2021-10-22 16:46:38 -0500

    employees.js complete

--- employees.js
+++ employees.js
@@ -20,7 +20,16 @@
 */
 
 //CODE HERE
+class Employee {
+    constructor(name, shifts) {
+        this.name = name;
+        this.shifts = shifts;
+    }
 
+    getSchedule = () => {
+        return `${this.name} works on these shifts: ${this.shifts}`
+    }
+}
 
 
 /*
@@ -34,14 +43,14 @@
 */
 
 //CODE HERE
-
+let empOne = new Employee("Jess", ["weekday mornings", "weekday afternoons"]);
 /*
     Call the `getSchedule` method on the
     `empOne` object.
 */
 
 //CODE HERE
-
+empOne.getSchedule();
 
 /*
     Make a copy of the empOne object
@@ -56,7 +65,8 @@
 */
 
 //CODE HERE
-
+let empTwo = {...empOne};
+empTwo.name = "Nick";
 
 
 //////////////////PROBLEM 2////////////////////
@@ -83,7 +93,20 @@
 */
 
 //CODE HERE
+class Manager extends Employee {
+    constructor(name, shifts, employees) {
+        super(name, shifts);
+        this.employees = employees;
+    }
 
+    getEmployees = () => {
+        return `${this.name} manages ${this.employees}`
+    }
+
+    addEmployee = emp => {
+        return this.employees.push(emp);
+    }
+}
 
 
 /*
@@ -98,7 +121,7 @@
 */
 
 //CODE HERE
-
+let manager = new Manager("Winston", ["weekday mornings", "weekday afternoons"], ["Cece", "Schmidt"])
 
 /*
     Call the `getEmployees` method on the
@@ -106,6 +129,7 @@
 */
 
 //CODE HERE
+manager.getEmployees();
 
 /*
     Call the `addEmployee` method on the 
@@ -114,6 +138,7 @@
 */
 
 //CODE HERE 
+manager.addEmployee("Coach");
 
 /*
     Call the `getEmployees` method on the
@@ -122,3 +147,5 @@
 */
 
 //CODE HERE
+manager.getEmployees();
+console.log(manager)
